0408604847174fac9c1036a2ae0eebcd01bb0d70
Chiara <milejanaja@gmail.com>
2022-03-14 14:29:43 +0900

    🎨🧚‍♀️ youtube tmp html added

---
+++ src/assets/js/application.js
@@ -0,0 +1,19 @@
+// // https://ics.media/entry/14771/
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  var elem = document.getElementById('target');
+  if (elem.innerHTML === '') {
+    elem.innerHTML = 'default';
+  } else {
+    console.log(elem);
+  }
+}
+
+function acceptTitle() {
+  const formElements = document.forms.info;
+  var arg1 = document.getElementById('target');
+  arg1.innerHTML = formElements.title.value;
+  init();
+}
+
