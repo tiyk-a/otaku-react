74859adfef04a8ce1caf1e434774bac3201573f4
Chiara <milejanaja@gmail.com>
2022-10-16 10:36:49 +0900

    🕊Add twitter accounts

--- src/containers/Twitter.jsx
+++ src/containers/Twitter.jsx
@@ -17,6 +17,9 @@ const Twitter = () => {
         <a className="twitter-timeline" data-height="1000" data-width="300" data-theme="dark" href="https://twitter.com/sexyz0neinfo?ref_src=twsrc%5Etfw">sexyz0neinfo</a>
         <a className="twitter-timeline" data-height="1000" data-width="300" href="https://twitter.com/naniwa_hayainfo?ref_src=twsrc%5Etfw">sexyz0neinfo</a>
         <a className="twitter-timeline" data-height="1000" data-width="300" data-theme="dark" href="https://twitter.com/princehayainfo?ref_src=twsrc%5Etfw">princehayainfo</a>
+        <a className="twitter-timeline" data-width="300" data-height="1000" href="https://twitter.com/kinki_hayainfo?ref_src=twsrc%5Etfw">kinki_hayainfo</a>
+        <a className="twitter-timeline" data-width="300" data-height="1000" data-theme="dark" href="https://twitter.com/jump_hayainfo?ref_src=twsrc%5Etfw">jump_hayainfo</a>
+        <a className="twitter-timeline" data-width="300" data-height="1000" data-theme="light" href="https://twitter.com/kanjaninfo?ref_src=twsrc%5Etfw">kanjaninfo</a>
     </div>
   );
 };
